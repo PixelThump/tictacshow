91784b4167e32ccc1c54a6bd4abd06949fde288d
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-10-02 15:39:40 +0200

    Update CommandProcessor.java

--- src/main/java/com/pixelthump/tictacshow/service/CommandProcessor.java
+++ src/main/java/com/pixelthump/tictacshow/service/CommandProcessor.java
@@ -4,5 +4,5 @@ import com.pixelthump.tictacshow.repository.model.TicTacShowState;
 import com.pixelthump.seshtypelib.repository.model.command.Command;
 
 public interface CommandProcessor {
-	void processCommand(Command command, TicTacShowState state);
+	TicTacShowState processCommand(Command command, TicTacShowState state);
 }
